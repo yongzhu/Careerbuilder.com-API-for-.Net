81eb3782be787bcbcd673ad74339acb4dce4ee78
Andrew Velez <andrewvelez@outlook.com>
2013-07-05 11:53:23 -0400

    preparing nuget package for release

---
+++ NuPKG/RELEASE_NOTES.md
@@ -0,0 +1,10 @@
+# CareerBuilder.com API Client Release Notes
+
+### 2.0.0.0
+
+* Major version bump because of the length of time since last release
+
+### 1.3.3.0
+
+* Add events that can be hooked for logging
+* Add timeout property to the API Interface to make it easy to change
--- Source/CBApi/Properties/AssemblyInfo.cs
+++ Source/CBApi/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -7,23 +8,24 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly: AssemblyTitle("CBApi")]
-[assembly: AssemblyDescription("A wrapper for the careerbuilder.com api")]
+[assembly: AssemblyDescription("CareerBuilder.com API C# Client Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jeffery Yeary")]
+[assembly: AssemblyCompany("CareerBuilder LLC")]
 [assembly: AssemblyProduct("CBApi")]
-[assembly: AssemblyCopyright("Copyright © Jeffery Yeary 2012")]
+[assembly: AssemblyCopyright("© CareerBuilder LLC, Jeffery Yeary")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
 [assembly: InternalsVisibleTo("CBApi.Tests")]
+[assembly: InternalsVisibleTo("CBApiExt")]
+[assembly: InternalsVisibleTo("CBApiExt.Tests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("53ccee65-4641-4e2c-9984-9d1059b59f5b")]
 
 // Version information for an assembly consists of the following four values:
@@ -36,8 +38,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: InternalsVisibleTo("CBApiExt")]
-[assembly: InternalsVisibleTo("CBApiExt.Tests")]
