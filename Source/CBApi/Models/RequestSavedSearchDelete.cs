5d77e2eba872d38199e8aaf292747fc2418b3db3
David-Sharpe <david.sharpe@careerbuilder.com>
2013-07-18 08:50:44 -0400

    Initial adding RequestSavedSearchDelete

---
+++ Source/CBApi/Models/RequestSavedSearchDelete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CBApi.Models
+{
+    [Serializable]
+    class RequestSavedSearchDelete
+    {
+        public string DeveloperKey { get; set; }
+        public string ExternalID { get; set; }
+        public string ExternalUserID { get; set; }
+        public String HostSite { get; set; }
+    }
+}
