5292040b8cda0c529ee954f8fd9acd9bfd3f3a49
David-Sharpe <david.sharpe@careerbuilder.com>
2013-07-26 09:17:08 -0400

    Changed list to use query string and not message body.

--- Source/CBApi/Framework/requests/SavedSearchListRequest.cs
+++ Source/CBApi/Framework/requests/SavedSearchListRequest.cs
@@ -12,24 +12,27 @@ namespace CBApi.Framework.Requests
         private string DeveloperKey { get; set; }
 
         public SavedSearchListRequest(APISettings settings)
-            : base(settings)
-        {
+            : base(settings) {
             DeveloperKey = settings.DevKey;
         }
 
-        public override string BaseUrl
-        {
+        public override string BaseUrl {
             get { return "/v1/SavedSearch/List"; }
         }
 
-        public SavedSearchListResponseModel Submit(SavedSearchListRequestModel search)
-        {
+        public SavedSearchListResponseModel Submit(SavedSearchListRequestModel search) {
             _request.AddBody(search);
-            base.BeforeRequest();
             search.DeveloperKey = DeveloperKey;
+            AddParametersToRequest(search);
+            base.BeforeRequest();
             IRestResponse<SavedSearchListResponseModel> response = _client.Execute<SavedSearchListResponseModel>(_request);
             CheckForErrors(response);
             return response.Data;
+        }
+
+        protected virtual void AddParametersToRequest(SavedSearchListRequestModel search) {
+            _request.AddParameter("developerkey", search.DeveloperKey);
+            _request.AddParameter("externaluserid", search.ExternalUserID);
         }
     }
 }
